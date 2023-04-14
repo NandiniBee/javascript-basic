2d45c80b2916b2e8d37c6b14762f0850b08f715c
NandiniBee <130660899+NandiniBee@users.noreply.github.com>
2023-04-14 11:26:36 +0000

    1st changes done

---
+++ basics/variable.js
@@ -0,0 +1,14 @@
+const userID = 45367
+var userPass ="asd546"
+let userEmail = "asd@gmail.com"
+
+// userID = 788777 
+userPass = "nnnas"
+userEmail ="cd@gmail.com"
+
+let userName
+
+/* var is not used becouse of block scop and funtional scop has issues */
+
+
+console.table([ userEmail, userID, userPass, userName ])
